df521592333824aa7595e42f318bf81f8d52c38e
seanwayland <seanwayland@gmail.com>
2018-02-25 12:30:27 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,9 @@
 /* Database connection settings */
 $host = 'localhost';
 $user = 'seanwayland';
-$pass = 'Goberheim1$';
+// password hidden by me! 
+$pass = '#######';
 $db = 'sean';
+// without the port number included it wouldn't connect properly ..
 $dbport = 3306;
 $mysqli = new mysqli($host,$user,$pass,$db,$dbport) or die($mysqli->error);
