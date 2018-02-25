842c32a11dd319293aaa71c624d9d239158fbfb6
seanwayland <seanwayland@gmail.com>
2018-02-25 12:29:32 -0500

    working login settings for DB connection

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+/* Database connection settings */
+$host = 'localhost';
+$user = 'seanwayland';
+$pass = 'Goberheim1$';
+$db = 'sean';
+$dbport = 3306;
+$mysqli = new mysqli($host,$user,$pass,$db,$dbport) or die($mysqli->error);
