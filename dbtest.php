0d9e7738338755df3827b7c38afd22acf655acfd
seanwayland <seanwayland@gmail.com>
2018-02-27 13:24:23 -0500

    system to read file from seperate php file

---
+++ dbtest.php
@@ -0,0 +1,31 @@
+<?php
+    /* Database connection settings */
+    $host = 'localhost';
+    $user = 'seanwayland';
+    $pass = 'Goberheim1$';
+    $db = 'sean';
+    $dbport = 3306;
+    $mysqli = new mysqli($host,$user,$pass,$db,$dbport) or die($mysqli->error);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+//<body>
+<body oncontextmenu="return false;">
+<?php
+$sql = ('SELECT URL FROM songs WHERE id=1');
+// get the result object.
+$result = $mysqli->query($sql);
+// Associative array
+$row=mysqli_fetch_assoc($result);
+
+?>
+
+<audio controls controlsList="nodownload noremoteplayback">
+<source src="mp3.php?id=1" type="audio/mpeg">
+//<source src="<?php echo $row["URL"]; ?>" type="audio/mpeg">
+</audio>
+</body>
+</html>
+
