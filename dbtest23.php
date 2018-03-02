daf351ee9791d015a3a8dece0282a5d0c210c823
seanwayland <seanwayland@gmail.com>
2018-03-02 14:21:10 -0500

    solution for streaming files with url in database

---
+++ dbtest23.php
@@ -0,0 +1,29 @@
+
+<?php
+    echo "hi sean";
+?>
+<!DOCTYPE html>
+<body oncontextmenu="return false;">
+<html>
+<head>
+</head>
+<body>
+CEM <br />
+<!-- play audio file but stop it from being downloadable -->
+<audio controls controlsList="nodownload noremoteplayback">
+<!-- get the source as a file from -->
+<source src="http://www.waylostreams.com/phptest/mp323.php?id=1" type="audio/mpeg">
+</audio>
+<br />
+EXCHANGE PLACE <br />
+<!-- play audio file but stop it from being downloadable -->
+<audio controls controlsList="nodownload noremoteplayback">
+<!-- get the source as a file from -->
+<source src="http://www.waylostreams.com/phptest/mp323.php?id=2" type="audio/mpeg">
+
+</body>
+</html>
+
+
+
+
