e03bc9eee297d4bcb8766ba47346217df468fab7
seanwayland <seanwayland@gmail.com>
2018-12-07 16:53:27 -0500

    removed <?php // include 'css/css.html'; ?> make header window smaller

--- playSong.php
+++ playSong.php
@@ -1,5 +1,5 @@
 
-
+<?php include 'pageHeader.php';?>
 
 <?php
 // this file updates the streams table in the database
@@ -118,7 +118,6 @@ else {
     $user = $result->fetch_assoc();
     $credits = $user['credits'];
 
-
     $song_id = $mysqli->escape_string($id);
     $result = $mysqli->query("SELECT * FROM songs WHERE id='$song_id'");
     $song = $result->fetch_assoc();
@@ -155,7 +154,7 @@ else {
 
 /* Displays user information and some useful messages */
 require 'db.php';
-session_start();
+//session_start();
 // Check if user is logged in using the session variable
 if ( $_SESSION['logged_in'] != 1 ) {
     $_SESSION['message'] = "You must log in before viewing your profile page!";
@@ -229,52 +228,11 @@ else {
 
 </style>
 <head>
-    <meta charset="UTF-8">
-    <title>WAYLOSTREAM <?= $first_name.' '.$last_name?></title>
-    <?php include 'css/css.html'; ?>
-</head>
+    <?php // include 'css/css.html'; ?>
+
 
 <body>
-<div class="form">
 
-    <h1>WAYLOSTREAM</h1>
-
-
-
-    <p>
-        <?php
-
-        // Display message about account verification link only once
-        if ( isset($_SESSION['message']) )
-        {
-            echo $_SESSION['message'];
-
-            // Don't annoy the user with more messages upon page refresh
-            unset( $_SESSION['message'] );
-        }
-
-        ?>
-    </p>
-
-    <?php
-
-    // Keep reminding the user this account is not active, until they activate
-    if ( !$active ){
-        echo
-        '<div class="info">
-              Account is unverified, please confirm your email by clicking
-              on the email link!
-              </div>';
-    }
-
-    ?>
-
-    <h2><?php echo $first_name.' '.$last_name; ?></h2>
-    <p><?= $email ?></p>
-
-    <a href="logout.php"><button class="button button-block" name="logout"/>Log Out</button></a>
-
-</div>
 
 <a href="http://www.waylostreams.com/login-system/buycredits.php">BUY CREDITS</a>
 <br />
